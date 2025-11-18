62f3ea0fd6da62c32436ec7e03b8881cf6d89e77
Shahid <97959691+shanialy@users.noreply.github.com>
2025-11-18 16:21:26 +0500

    fix: update webhook revenue cat

--- src/controllers/revenueCatWebhook.controller.ts
+++ src/controllers/revenueCatWebhook.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import UserModel from "../models/UserModel";
+import mongoose from "mongoose";
 
 export const handleRevenueCatWebhook = async (req: Request, res: Response) => {
   try {
@@ -8,13 +9,13 @@ export const handleRevenueCatWebhook = async (req: Request, res: Response) => {
     const { event } = json;
     const userId = event.app_user_id || event.original_app_user_id;
     console.log("USERID", userId)
+    const convertedId = new mongoose.Types.ObjectId(userId)
     // const userId = "6907ceca3f4c940d47257ea6";
     const eventType = event.type || event.event;
     console.log("EVENT TYPE",eventType)
     // const eventType = "INITIAL_PURCHASE";
-    // const userId = subscriber?.subscriber_id;
 
-    const user: any = await UserModel.findById(userId);
+    const user: any = await UserModel.findById(convertedId);
     if (!user) return res.status(404).send("User not found");
 
     if (eventType === "INITIAL_PURCHASE" || eventType === "RENEWAL") {
