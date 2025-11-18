85e2e0399a086aafb8e108f7bb7fea2e5c16e701
Shahid <97959691+shanialy@users.noreply.github.com>
2025-11-18 17:26:01 +0500

    fix: webhook fixes

--- src/controllers/revenueCatWebhook.controller.ts
+++ src/controllers/revenueCatWebhook.controller.ts
@@ -1,26 +1,33 @@
 import { Request, Response } from "express";
 import UserModel from "../models/UserModel";
-import mongoose from "mongoose";
 
 export const handleRevenueCatWebhook = async (req: Request, res: Response) => {
   try {
     const json = JSON.parse(req.body.toString());
     console.log("RevenueCat Webhook received:", json);
-    const { event } = json;
-    const userId = event.app_user_id || event.original_app_user_id;
+    const { event }:any = json;
+    let userId;
+    if (event.type === "TRANSFER") {
+      userId = event.transferred_to?.[1] || null;
+    } else {
+      userId = event.app_user_id || event.original_app_user_id;
+    }
     console.log("USERID", userId)
-    const convertedId = new mongoose.Types.ObjectId(userId)
-    const eventType = event.type
-    console.log("EVENT TYPE",eventType)
+    console.log("EVENT TYPE", event.type)
 
-    const user: any = await UserModel.findById(convertedId);
+    const user: any = await UserModel.findById(userId);
+
     if (!user) return res.status(404).send("User not found");
 
-    if (eventType === "INITIAL_PURCHASE" || eventType === "RENEWAL") {
+    if (
+      event.type === "INITIAL_PURCHASE" ||
+      event.type === "RENEWAL" ||
+      event.type === "TRANSFER"
+    ) {
       user.type = "PREMIUM";
     } else if (
-      eventType === "CANCELLATION" ||
-      eventType === "DID_FAIL_TO_RENEW"
+      event.type === "CANCELLATION" ||
+      event.type === "DID_FAIL_TO_RENEW"
     ) {
       user.type = "FREEMIUM";
     }
